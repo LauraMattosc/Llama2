7b3e18d46a2f47e59c65c4c00fa0a94637b5f957
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2024-03-25 09:58:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ llm = 'meta/llama-2-7eb-chat:d21985930eff70f5a87c7f467e6feeaa3efb71f166725eae396
 temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
 top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
 max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
+repetition_penalty = st.sidebar.slider('Repetition Penalty', min_value=1.0, max_value=2.0, value=1.1, step=0.1)
 
 st.markdown("📘 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot)")
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
