2b37df171f2e779c746631764e69c49be1f9d580
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2024-03-25 13:34:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ with st.sidebar:
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
-    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Models and parameters')
     selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
